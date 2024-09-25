5c72a4810aeed1c45b82dc719c760d542c3ffb3e
Official-Krish <krishanand0103@gmail.com>
2024-09-26 00:58:02 +0530

    [ui]: footer fix

--- apps/Client/src/components/footer.tsx
+++ apps/Client/src/components/footer.tsx
@@ -1,14 +1,29 @@
+import { Instagram, Linkedin, Youtube } from "lucide-react"
+import { Link } from "react-router-dom"
+
 export const Footer = () => {
     return <div>
-        <footer className="w-full py-6 bg-gray-950 border-t border-gray-800 pb-10">
-            <div className="container px-4 md:px-6 max-w-6xl mx-auto flex flex-col sm:flex-row justify-between items-center">
-                <p className="text-xs text-gray-400">© 2024 CoDevSpace. All rights reserved.</p>
-                <nav className="flex gap-4 sm:gap-6 mt-4 sm:mt-0">
-                    <div className="text-xs hover:underline underline-offset-4 hover:text-emerald-400 transition-colors text-white">
+        <footer className="w-full py-6 bg-gray-950 border-t border-gray-800 h-60">
+            <div className="px-4 md:px-6 max-w-6xl mx-auto flex flex-col sm:flex-row justify-between items-center">
+                <div>
+                    <p className="text-lg text-gray-400">© 2024 CoDevSpace. All rights reserved.</p>
+                    <div className="pt-10 text-white font-bold pb-3"> Follow us</div>
+                    <div className="flex gap-4">
+                        <Link to=""><Youtube className="h-6 w-6 text-white" /></Link>
+                        <Link to=""><Linkedin className="h-6 w-6 text-white" /></Link>
+                        <Link to=""><Instagram className="h-6 w-6 text-white" /></Link>
+                    </div>
+                </div>
+                <nav className="gap-4 sm:gap-6">
+                    <p className="text-md text-white pb-3 font-bold">CoDevSpace Legal</p>
+                    <div className="text-sm hover:underline underline-offset-4 hover:text-emerald-400 transition-colors text-gray-400 cursor-pointer pb-1">
                         Terms of Service
                     </div>
-                    <div className="text-xs hover:underline underline-offset-4 hover:text-emerald-400 transition-colors text-white">
-                        Privacy
+                    <div className="text-sm hover:underline underline-offset-4 hover:text-emerald-400 transition-colors text-gray-400 cursor-pointer pb-1">
+                        Privacy & Policy
+                    </div>
+                    <div className="text-sm hover:underline underline-offset-4 hover:text-emerald-400 transition-colors text-gray-400 cursor-pointer pb-1">
+                        Refund & Cancellation
                     </div>
                 </nav>
             </div>
